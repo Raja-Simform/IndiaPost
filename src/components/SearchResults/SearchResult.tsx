8ffb4f453cca3399376c57ff9e0e049077ee8ac1
Raja-Simform <raja.jha@simformsolutions.com>
2025-05-16 16:29:26 +0530

    feat:add sort

--- src/components/SearchResults/SearchResult.tsx
+++ src/components/SearchResults/SearchResult.tsx
@@ -1,63 +1,62 @@
-import type { SearchProps } from "./SearchResultTypes";
+import { useState } from "react";
+import type { FilterProps, SearchProps } from "./SearchResultTypes";
+import SearchResultRender from "./SearchResultRender";
 
 export default function SearchResult({ data, CODE }: SearchProps) {
+  const [filter, setFilter] = useState<FilterProps>({ delivery: "all", branchType: "all" });
+  const [selectedDelivery, setSelectedDelivery] = useState("all");
+  const [selectedBranchType, setSelectedBranchType] = useState("all");
+
+  function handleSort() {
+    setFilter({
+      delivery: selectedDelivery,
+      branchType: selectedBranchType,
+    });
+  }
+  const filteredPostOffices = data.PostOffice?.filter((office) => {
+    const deliveryMatch =
+      filter.delivery === "all" || office.DeliveryStatus === filter.delivery;
+    const branchTypeMatch =
+      filter.branchType === "all" || office.BranchType === filter.branchType;
+    return deliveryMatch && branchTypeMatch;
+  });
   return (
     <div className="flex-wrap mx-auto p-6 bg-gradient-to-r from-indigo-50 via-purple-50 to-pink-50 min-h-screen">
+      <div className="flex justify-center">
+        <select
+          name="Delivery"
+          className=" border-2  p-2 r"
+          value={selectedDelivery}
+          onChange={(e) => setSelectedDelivery(e.target.value)}
+        >
+          <option value="all">Delivery Staus(All)</option>
+          <option value="Delivery">Delivery</option>
+          <option value="Non-Delivery">Non-Delivery</option>
+        </select>
+        <select
+          name="BranchType"
+          id=""
+          className=" ml-2 border-2  p-2 "
+          value={selectedBranchType}
+          onChange={(e) => setSelectedBranchType(e.target.value)}
+        >
+          <option value="all">BranchType(All)</option>
+          <option value="Post Office">Branch PostOffice</option>
+          <option value="Sub Post Office">Sub PostOffice</option>
+        </select>
+        <button
+          onClick={handleSort}
+          className="ml-2 px-6 py-2 bg-blue-600 text-white rounded font-semibold "
+        >
+          Sort
+        </button>
+      </div>
       <h2 className="text-3xl font-semibold mb-6 text-purple-700 border-b-2 border-purple-300 pb-2">
         Post Offices : {CODE}
       </h2>
       <p className="mb-6 italic text-gray-700 ">{data.Message}</p>
 
-      <div className="grid gap-8 md:grid-cols-3">
-        {data.PostOffice?.map((office) => (
-          <div
-            key={office.Name}
-            className="border border-purple-300 rounded-xl p-6 shadow-md hover:shadow-xl transition-shadow bg-white text-left "
-          >
-            <h3 className="text-2xl font-extrabold mb-3 text-purple-800 text-center">
-              {office.Name}
-            </h3>
-            <div className="flex justify-around">
-            <div> <p className="mb-2">
-              <span className="font-semibold text-purple-600">Branch Type:</span>{" "}
-              {office.BranchType}
-            </p>
-            <p className="mb-2">
-              <span className="font-semibold text-purple-600">
-                Delivery Status:
-              </span>{" "}
-              {office.DeliveryStatus}
-            </p>
-            <p className="mb-2">
-              <span className="font-semibold text-purple-600">District:</span>{" "}
-              {office.District}
-            </p>
-            <p className="mb-2">
-              <span className="font-semibold text-purple-600">Division:</span>{" "}
-              {office.Division}
-            </p></div>
-            <div><p className="mb-2">
-              <span className="font-semibold text-purple-600">Region:</span>{" "}
-              {office.Region}
-            </p>
-            <p className="mb-2">
-              <span className="font-semibold text-purple-600">State:</span>{" "}
-              {office.State}
-            </p>
-            <p className="mb-2">
-              <span className="font-semibold text-purple-600">Circle:</span>{" "}
-              {office.Circle}
-            </p>
-
-            {office.Description && (
-              <p className="mt-4 text-gray-600 italic border-t pt-3">
-                {office.Description}
-              </p>
-            )}</div>
-            </div>
-          </div>
-        ))}
-      </div>
+       <SearchResultRender filteredPostOffices={filteredPostOffices}/>
     </div>
   );
 }
--- src/components/SearchResults/SearchResultTypes.tsx
+++ src/components/SearchResults/SearchResultTypes.tsx
@@ -1,23 +1,34 @@
 export interface PostOffice {
-    Name: string;
-    Description: string;
-    BranchType: string;
-    DeliveryStatus: string;
-    Circle: string;
-    District: string;
-    Division: string;
-    Region: string;
-    State: string;
-    Country: string;
-  }
-  
-  export interface SearchData {
-    Message: string;
-    Status: string;
-    PostOffice: PostOffice[] | null;
-  }
-  
-export interface SearchProps {
-    CODE: number | string;
-    data:SearchData;
+  Name: string;
+  Description: string;
+  BranchType: string;
+  DeliveryStatus: string;
+  Circle: string;
+  District: string;
+  Division: string;
+  Region: string;
+  State: string;
 }
+
+export interface SearchData {
+  Message: string;
+  Status: string;
+  PostOffice: PostOffice[] | null;
+}
+
+export interface FilterProps {
+  delivery: string;
+  branchType: string;
+}
+
+export type SearchProps = {
+  data: {
+    PostOffice?: PostOffice[];
+    Message: string;
+  };
+  CODE: string;
+};
+
+export type SearchResultRenderProps = {
+  filteredPostOffices?: PostOffice[];
+};
